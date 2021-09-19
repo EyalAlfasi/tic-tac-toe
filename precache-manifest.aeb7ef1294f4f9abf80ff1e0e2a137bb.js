801c9ca2c12302364e6c983e5ccb3afa99b9c81e
EyalAlfasiSkills <eyala@academy-skills.com>
2021-09-19 04:43:35 +0300

    gh-pages

---
+++ precache-manifest.aeb7ef1294f4f9abf80ff1e0e2a137bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4f24f76e86c719a4d5",
+    "url": "/tic-tac-toe/css/app.97a79815.css"
+  },
+  {
+    "revision": "a103e644ab47c2b5573b73bf46c99384",
+    "url": "/tic-tac-toe/img/logo.a103e644.svg"
+  },
+  {
+    "revision": "b5cc2ed0e5ad68903d2be8a73366327e",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "6c4f24f76e86c719a4d5",
+    "url": "/tic-tac-toe/js/app.98ca0f9c.js"
+  },
+  {
+    "revision": "fb25f2a470f381ff9198",
+    "url": "/tic-tac-toe/js/chunk-vendors.7e1bb2fd.js"
+  },
+  {
+    "revision": "49ef5d73b56ab519e736ff31343a786b",
+    "url": "/tic-tac-toe/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tic-tac-toe/robots.txt"
+  }
+]);
